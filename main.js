cb0887aac4b770e584341eaa31c60dd7ca1502a8
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-04-28 01:39:30 +0200

    Finish the design

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+const btn = document.getElementById('menu-btn');
+const mobileNav = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open');
+  mobileNav.classList.toggle('hidden');
+});
